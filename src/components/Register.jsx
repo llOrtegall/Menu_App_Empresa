cfa6029f4d6994a3d56e69f6ef63e67eea43da50
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-06 10:54:53 -0500

    pass and login

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react"
+
 export function Register() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>Register And Login</div>
+    <section className="bg-blue-200 h-screen flex items-center">
+      <form className="w-64 mx-auto">
+        <input className="block w-full rounded-sm p-2 mb-2 border"
+          value={username} onChange={ev => setUsername(ev.target.value)}
+          type="text"
+          placeholder="Usuario" />
+        <input className="block w-full rounded-sm p-2 mb-2 border"
+          value={password} onChange={ev => setPassword(ev.target.value)}
+          type="password"
+          placeholder="Contraseña" />
+        <button className="bg-blue-500 text-white block w-full rounded-md p-2">Register</button>
+      </form>
+    </section>
   )
 }
