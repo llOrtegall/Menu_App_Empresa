e0ae4e90db94cab3d1f5c735831edcf87ba2d7d8
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-06 11:23:18 -0500

    crear secret key

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { UserModel } from "./models/User.js"
+import jwt from "jsonwebtoken";
 
 dotenv.config()
 const mongoUrl = process.env.MONGO_URL;
@@ -13,10 +15,12 @@ app.get('/test', (req, res) => {
   res.json('test ok')
 })
 
-app.post('/register', (req, res) => {
-
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const createdUser = await UserModel.create({ username, password })
+  jwt.sign({ userId: createdUser._id },)
 })
 
 app.listen(PORT, () => {
-  console.log("Server Running Port On http://localhost:" + PORT)
+  console.log("Server Running On Port http://localhost:" + PORT)
 })
