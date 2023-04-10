55a9198f01cf8db06546b08ca46e86f4d9ff691f
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-10 08:10:22 -0500

    haciendo funcionar cookie

--- api/index.js
+++ api/index.js
@@ -4,37 +4,48 @@ import mongoose from "mongoose";
 import { UserModel } from "./models/User.js";
 import jwt from "jsonwebtoken";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 
 const PORT = 4000;
 dotenv.config();
 
 const MONGO_URL = process.env.MONGO_URL;
-const jwtSecret = process.env.JWT_SECRET;
+const JWT_SECRECT = process.env.JWT_SECRECT;
 const ORIGIN = process.env.CLIENT_URL;
 
 mongoose.connect(MONGO_URL);
 const app = express();
-
-app.use(cors({
-  credentials: true,
-  origin: ORIGIN
-}));
+app.use(cors({ credentials: true, origin: ORIGIN }));
+app.use(cookieParser());
 app.use(express.json());
 
 app.get('/test', (req, res) => {
   res.json('test ok')
 });
 
+app.get('/profile', (req, res) => {
+  const token = req.cookies.token
+  if (token) {
+    jwt.verify(token, JWT_SECRECT, {}, (err, userData) => {
+      if (err) throw err;
+      res.json(userData);
+    });
+  } else {
+    res.status(401).json('No Token')
+  }
+});
+
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
   try {
     const createdUser = await UserModel.create({ username, password });
-    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+    jwt.sign({ userId: createdUser._id }, JWT_SECRECT, {}, (err, token) => {
       if (err) { throw err }
       else {
         res.cookie('token', token).status(201).json({
-          id: createdUser._id
+          id: createdUser._id,
+          username
         })
       }
     })
