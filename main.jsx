a86e2e721404dbd3e061a251c6825d274b98b39c
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-05-06 18:43:43 -0500

    antes del tailwin css

--- main.jsx
+++ main.jsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from './src/App.jsx';
+import App from "./src/App.jsx";
 
-const root = createRoot(document.getElementById('root'));
-
-root.render(
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>
 );
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Products</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Cont Us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
