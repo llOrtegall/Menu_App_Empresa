f959805d3330315c35de6670e1eed41c86f81eb0
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-07-10 11:40:32 -0500

    export const aApp

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 export const App = () => {
   return (
-    <nav className="w-full h-5 bg-blue-500">
-      <ul>
+    <nav className="w-full  bg-blue-500 p-2">
+      <ul className="flex justify-around">
         <li><a href="">Home</a></li>
-        <li><a href="">Home</a></li>
+        <li><a href="">Contac</a></li>
         <li><a href="">Home</a></li>
         <li><a href="">Home</a></li>
       </ul>
